ed092aa1a14b9cea39e592fe03d42a10dbb33e76
brainiac <brainiac2k@gmail.com>
2020-12-12 22:11:59 -0800

    Update eqmain offsets for new patch

--- eqgame.h
+++ eqgame.h
@@ -1278,17 +1278,17 @@
 // eqmain.dll offsets
 //
 
-#define EQMain__CEQSuiteTextureLoader__GetTexture_x                0x100899C0
-#define EQMain__CLoginViewManager__HandleLButtonUp_x               0x100198D0
-#define EQMain__CXWndManager__GetCursorToDisplay_x                 0x1007BD30
-#define EQMain__LoginController__FlushDxKeyboard_x                 0x100151D0
-#define EQMain__LoginController__GiveTime_x                        0x10015230
-#define EQMain__LoginServerAPI__JoinServer_x                       0x10016D30
+#define EQMain__CEQSuiteTextureLoader__GetTexture_x                0x1008AD30
+#define EQMain__CLoginViewManager__HandleLButtonUp_x               0x10019910
+#define EQMain__CXWndManager__GetCursorToDisplay_x                 0x1007CF30
+#define EQMain__LoginController__FlushDxKeyboard_x                 0x10015240
+#define EQMain__LoginController__GiveTime_x                        0x100152A0
+#define EQMain__LoginServerAPI__JoinServer_x                       0x10016D60
 #define EQMain__WndProc_x                                          0x1000BEA0
 
-#define EQMain__pinstCEQSuiteTextureLoader_x                       0x10165D38
-#define EQMain__pinstCLoginViewManager_x                           0x1016C3B4
-#define EQMain__pinstCXWndManager_x                                0x1037E8AC
-#define EQMain__pinstCSidlManager_x                                0x1037E8B0
-#define EQMain__pinstLoginController_x                             0x1016C3B8
-#define EQMain__pinstLoginServerAPI_x                              0x1016C3A8
+#define EQMain__pinstCEQSuiteTextureLoader_x                       0x10167D38
+#define EQMain__pinstCLoginViewManager_x                           0x1016E3B4
+#define EQMain__pinstCXWndManager_x                                0x103808AC
+#define EQMain__pinstCSidlManager_x                                0x103808B0
+#define EQMain__pinstLoginController_x                             0x1016E3B8
+#define EQMain__pinstLoginServerAPI_x                              0x1016E3A8
