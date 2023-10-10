ae198d5df886fa82231c8660bf799cef1b20ae4d
brainiac <brainiac2k@gmail.com>
2023-10-09 23:47:04 -0700

    Add HAS_DIRECTX_9 / HAS_DIRECTX_11

--- BuildType.h
+++ BuildType.h
@@ -68,6 +68,10 @@
 // Autoskills was added April 19, 2017
 #define HAS_AUTOSKILLS IS_CLIENT_DATE(20170419)
 
+// DirectX 11 Support added to Test on 9/12/2023
+#define HAS_DIRECTX_11 (IS_CLIENT_DATE(20230912) && IS_TEST_CLIENT)
+#define HAS_DIRECTX_9 (!HAS_DIRECTX_11)
+
 // Dragon Hoard was added with the Claws of Veeshan Expansion
 #define HAS_DRAGON_HOARD IS_EXPANSION_LEVEL(EXPANSION_LEVEL_COV)
 
