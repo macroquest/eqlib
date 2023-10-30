0f3cf1f947e39cc11baed9e3fbce8dfd2889ae5b
ChatWithThisName <jesse.m.bevil.mil@gmail.com>
2023-10-30 01:19:54 -0500

    Add defines for expansion Laurion's Song (#13)
    Allows use of the check prior to the expansion release as well as enables a quick transition from current expansion level to the new expansion level for test and live branches.

--- Constants.h
+++ Constants.h
@@ -663,6 +663,7 @@ enum EQExpansion
 	EQExpansionCOV                  = EXPANSION_LEVEL_COV,
 	EQExpansionTOL                  = EXPANSION_LEVEL_TOL,
 	EQExpansionNOS                  = EXPANSION_LEVEL_NOS,
+	EQExpansionLS                   = EXPANSION_LEVEL_LS,
 };
 
 #define EQ_EXPANSION(x)             (1 << (x - 1))
@@ -698,6 +699,7 @@ enum EQExpansion
 #define EXPANSION_COV               EQ_EXPANSION(EXPANSION_LEVEL_COV)
 #define EXPANSION_TOL               EQ_EXPANSION(EXPANSION_LEVEL_TOL)
 #define EXPANSION_NOS               EQ_EXPANSION(EXPANSION_LEVEL_NOS)
+#define EXPANSION_LS                EQ_EXPANSION(EXPANSION_LEVEL_LS)
 
 // Enumeration defining expansion bit mask representing expansion flags. Often used to
 // determine expansion ownership or requirements.
@@ -733,6 +735,7 @@ enum EQExpansionOwned
 	EQExpansionCOVOwned             = EXPANSION_COV,
 	EQExpansionTOLOwned             = EXPANSION_TOL,
 	EQExpansionNOSOwned             = EXPANSION_NOS,
+	EQExpansionLSOwned              = EXPANSION_LS,
 
 	// Mask representing all expansions
 	EQExpansionHighestOwnedPlusOne_,
